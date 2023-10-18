73350370ef5dac4a547b3fd6769ee0d9eb3b253c
rbrtj <robertjaszczurek@icloud.com>
2023-10-18 16:00:26 +0200

    register schema

--- src/common/validation/auth.ts
+++ src/common/validation/auth.ts
@@ -4,3 +4,19 @@ export const loginSchema = z.object({
   username: z.string().min(1, "Nazwa użytkownika jest wymagana").max(30),
   password: z.string().min(8, "Hasło musi mieć co najmniej 8 znaków").max(30),
 });
+
+export const registerSchema = z
+  .object({
+    username: z.string().min(1, "Nazwa użytkownika jest wymagana").max(30),
+    // TODO: Add validation for @pk.edu.pl email
+    email: z
+      .string()
+      .min(1, "Adres email jest wymagany")
+      .email("Nieprawidłowy adres email"),
+    password: z.string().min(8, "Hasło musi składać się z conajmniej 8 znaków"),
+    confirmPassword: z.string().min(1, "Hasła nie są identyczne"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Hasła nie są identyczne",
+  });
